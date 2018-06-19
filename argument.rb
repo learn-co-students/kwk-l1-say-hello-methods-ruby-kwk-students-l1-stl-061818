73182341bc96e49b2f4a2c0173b4611f9d132ac0
btokraks22 <bree.v.v.13@gmail.com>
2018-06-19 15:24:02 +0000

    Done.

---
+++ argument.rb
@@ -0,0 +1,5 @@
+def greeting(local_greeting="Hello", your_name, my_name, time_of_day)
+  puts "#{local_greeting} #{your_name} I'm #{my_name}. The time is #{time_of_day}"
+end
+
+greeting("Rhianna", "Drake", "10:00am")
